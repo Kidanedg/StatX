1bde1094907154de409986b3668590a421bda549
Kidanedg <gkidanedesta@gmail.com>
2026-03-15 09:14:30 -0700

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ import numpy as np
 # ------------------------------------------------
 # IMPORT STATX MODULE REGISTRY
 # ------------------------------------------------
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 import modules as statx
 
